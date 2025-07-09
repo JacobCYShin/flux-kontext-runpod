dfe16bc6d8656f67d8f33f9a9c80b93f1dbd1220
visionhong <tjems6498@gmail.com>
2025-07-09 14:03:04 +0900

    refactor: use base64 encoding decoding

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from io import BytesIO
+import base64
+from PIL import Image
 import torch
+import runpod
+from runpod.serverless.utils.rp_validator import validate
 from diffusers import FluxKontextPipeline
 from diffusers.utils import load_image
 from nunchaku import NunchakuFluxTransformer2dModel
 from nunchaku.utils import get_precision
-import runpod
-from runpod.serverless.utils.rp_validator import validate
-from io import BytesIO
-import base64
+
 
 schema = {
     "image": {
@@ -30,10 +32,23 @@ def load_model():
 
     return pipeline
 
-def image_to_base64(image):
+def encode_image_to_base64(image):
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+def decode_base64_to_image(base64_str):
+    """
+    Decodes a base64 string to an image.
+    """
+    if "," in base64_str:
+        base64_str = base64_str.split(',')[1]
+
+    try:
+        image_data = base64.b64decode(base64_str)
+        return Image.open(BytesIO(image_data))
+    except Exception as e:
+        raise ValueError(f"Invalid base64 string: {e}")
 
 def handler(event):
     try:
@@ -46,14 +61,19 @@ def handler(event):
         if "model" not in globals():
             model = load_model()
 
-        image = validated_input["validated_input"]["image"]
+        image_source = validated_input["validated_input"]["image"]
         prompt = validated_input["validated_input"]["prompt"]
 
-        image = load_image(image).convert("RGB")
-        image = model(image=image, prompt=prompt, guidance_scale=2.5).images[0]
+        if image_source.startswith(("http://", "https://")):
+            input_image = load_image(image_source)
+        else:
+            input_image = decode_base64_to_image(image_source)
+
+        input_image = input_image.convert("RGB")
+        output_image = model(image=input_image, prompt=prompt, guidance_scale=2.5).images[0]
         
         # Convert the image to base64
-        image_base64 = image_to_base64(image)
+        image_base64 = encode_image_to_base64(output_image)
 
 
         job_result = {
